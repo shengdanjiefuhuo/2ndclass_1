d373eeb44d8e39f5f8c599b8bff53f20698b782a
yk h <904771211@qq.com>
2020-09-28 18:13:03 +0800

    .

---
+++ 2ndclass_1/no1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+int main()
+{
+	cv::Mat srcMat = imread("C:/Users/hp/Desktop/2.jpg");
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+		
+
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
+}
